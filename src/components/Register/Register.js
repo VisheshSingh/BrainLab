77a9d1d5873b181a3e91207eaab60116225069d4
Vishesh <visheshsingh16@gmail.com>
2018-12-14 22:39:18 -0500

    refactoring and cleanup

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -4,53 +4,53 @@ const Register = props => {
   const { onRouteChange } = props;
   return (
     <div className="grow br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-      <main class="pa4 black-80">
-        <form class="measure center">
-          <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
-            <legend class="f4 fw6 ph0 mh0">Register</legend>
-            <div class="mt3">
-              <label class="db fw6 lh-copy f6" for="name">
+      <main className="pa4 black-80">
+        <div className="measure center">
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f4 fw6 ph0 mh0">Register</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="name">
                 Name
               </label>
               <input
-                class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                 type="email"
                 name="name"
                 id="name"
               />
             </div>
-            <div class="mt3">
-              <label class="db fw6 lh-copy f6" for="email-address">
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">
                 Email
               </label>
               <input
-                class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                 type="email"
                 name="email-address"
                 id="email-address"
               />
             </div>
-            <div class="mv3">
-              <label class="db fw6 lh-copy f6" for="password">
+            <div className="mv3">
+              <label className="db fw6 lh-copy f6" htmlFor="password">
                 Password
               </label>
               <input
-                class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                 type="password"
                 name="password"
                 id="password"
               />
             </div>
           </fieldset>
-          <div class="">
+          <div className="">
             <input
               onClick={() => onRouteChange("home")}
-              class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
               type="submit"
               value="Register"
             />
           </div>
-        </form>
+        </div>
       </main>
     </div>
   );
